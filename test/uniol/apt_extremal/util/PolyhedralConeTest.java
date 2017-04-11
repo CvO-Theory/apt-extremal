ad076eba7c2f732f3c2ac77b2dcb1cdaf70ab66b
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2017-04-11 11:18:59 +0200

    PolyhedralCone: Add a test for an error I already fixed yesterday
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

--- test/uniol/apt_extremal/util/PolyhedralConeTest.java
+++ test/uniol/apt_extremal/util/PolyhedralConeTest.java
@@ -19,17 +19,41 @@
 
 package uniol.apt_extremal.util;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+@SuppressWarnings("unchecked")
 public class PolyhedralConeTest {
+	private final static BigInteger ZERO = BigInteger.ZERO;
+	private final static BigInteger ONE = BigInteger.ONE;
+
 	@Test
 	public void testTrivialEquality() {
 		// This used to provoke an IllegalArgumentException from polco
 		PolyhedralCone cone = new PolyhedralCone(2);
 		cone.addEquation(0, 0);
 		assertThat(cone.findExtremalRays(), empty());
+	}
+
+	@Test
+	public void testBigIntError()
+	throws Throwable {
+		// This test fails when the arithmetic is set to bigint
+		PolyhedralCone cone = new PolyhedralCone(3);
+		cone.addEquation(0, 2, -2);
+		cone.addInequality(1, 0, 0);
+		cone.addInequality(0, 1, 0);
+		cone.addInequality(0, 0, 1);
+		cone.addInequality(1, 0, -1);
+		cone.addInequality(1, 1, -2);
+
+		assertThat(cone.findExtremalRays(), containsInAnyOrder(
+					Arrays.asList(ONE, ZERO, ZERO),
+					Arrays.asList(ONE, ONE, ONE)));
 	}
 }
 
